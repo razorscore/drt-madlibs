d769d0ab751d72c58a3834e1ab70f0bc63efd753
Daven Triplett <daventriplett@gmail.com>
2018-07-17 10:57:41 -0600

    made a button to toggle content visibility

--- src/components/card.js
+++ src/components/card.js
@@ -27,14 +27,22 @@ class Card extends Component {
             adjectiveFour: '',
             nounThree: '',
             celebFour: '',
-            adjectiveFive: ''
+            adjectiveFive: '',
+
+            contentVisible: false
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault()
+        this.setState({contentVisible: !this.state.contentVisible});
     }
 
     render() {
@@ -63,12 +71,17 @@ class Card extends Component {
 
 
         return (
-            <div className="card">
+            <form onSubmit={this.handleFormSubmit} className="card">
+                <div className="card__inputs">
                 {
                     inputData.map(data => Input((data), this.handleInputChange))
                 }
-                <Content data={this.state}/>
-            </div>
+                </div>
+                <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
+                {
+                    this.state.contentVisible ? <Content data={this.state}/> : ''
+                }
+            </form>
         )
     }
 }
