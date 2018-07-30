2a30efca1d3d1f94259c19bb602c3a12f4baf54d
Daven Triplett <daventriplett@gmail.com>
2018-07-30 10:55:09 -0600

    css clip path

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="header__skew"></div>
+            <div className="header__border"></div>
             <h1>DRT Madlibs</h1>
             <p>Fill out the fields below and click the  generate button to see the Mad Lib story.</p>
         </div>
