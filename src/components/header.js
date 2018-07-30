1d1e8072212258fdc36e0951c48405749704e4f9
Daven Triplett <daventriplett@gmail.com>
2018-07-30 09:53:37 -0600

    Google no work for css clip path??

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
+            <div className="header__skew"></div>
             <h1>DRT Madlibs</h1>
             <p>Fill out the fields below and click the  generate button to see the Mad Lib story.</p>
         </div>
